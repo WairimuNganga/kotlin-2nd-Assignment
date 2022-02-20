735a7b78443a499d117e8cb30f9f8bf3a092df5a
WairimuNganga <wairimuznganga@gmail.com>
2022-02-20 16:53:26 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,26 @@
+fun main(){
+    var greeting=printName("Sally")
+    var result=giveModulus(11,3)
+    println(result)
+    var total=getSum(36,52,12,32)
+    println(total)
+    var hobby= println("I love exercising")
+}
+fun printName(name:String){
+    var greeting="Hello $name"
+    println(greeting)
+}
+fun giveModulus(num1:Int,num2:Int):Int{
+    var modulus =num1 % num2
+    return modulus
+}
+fun getSum(num1:Int,num2: Int,num3:Int,num4:Int) :Int{
+    var addition =num1 + num2 + num3 + num4
+    return addition
+}fun funFact(text:String){
+    var text = String
+}
+
+
+
+
